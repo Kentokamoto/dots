cce4b0c640e5e40c6bdfb6a57e6c9e25a12d7ffe
Kento Okamoto <oka.keno@gmail.com>
2017-04-11 11:27:37 -0600

    i3 modified with screenshots

--- Linux/.config/wallpaper.sh
+++ Linux/.config/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-	find ~/Pictures/'Desktop Backgrounds' -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
+	find ~/Google_Drive/Photos/'Desktop Backgrounds' -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
 	shuf -n1 -z | xargs -0 feh --bg-fill
 	sleep 15m
 done
