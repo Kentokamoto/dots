fda13d7a3346c7e8fdb1c0322175d9b3881bdf65
Kento Okamoto <kentokamoto@protonmail.com>
2022-12-02 09:57:48 -0800

    Formatting

--- .config/nvim/lua/kento/plugins-setup.lua
+++ .config/nvim/lua/kento/plugins-setup.lua
@@ -1,13 +1,13 @@
 --- Install Packer if not yet installed
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -23,7 +23,7 @@ vim.cmd([[
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
 	use("shaunsingh/nord.nvim")
-    use("catppuccin/nvim")
+	use("catppuccin/nvim")
 	use("christoomey/vim-tmux-navigator")
 	-- File Explorer
 	use("nvim-tree/nvim-tree.lua")
